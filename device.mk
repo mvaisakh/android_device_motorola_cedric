7ed5f4d7e3326474400b5d35f4a14178d6cd6398
Vachounet <vachounet@live.fr>
2018-05-16 13:53:45 +0200

    cedric: build camera provider service
    * And add hidl to manifest
    * Also add memtrack service
    
    Signed-off-by: mvaisakh <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -111,7 +111,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    libjustshoot_shim
+    libjustshoot_shim \
+    android.hardware.camera.provider@2.4-service \
+    libshim_camera_hal \
+    libshims_camera \
+    camera.msm8937
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/msm8937_mot_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8937_mot_camera.xml \
